5796f2894cd48903ddd3fef55b8cab58c25640fe
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-01 15:57:02 +0200

    add sign up

---
+++ src/pages/signUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../components/Nav';
+const singUp = () => {
+  return (
+    <>
+      <Nav />
+      <p>Create your count please !</p>
+      </>
+  )
+}
+
+export default singUp;
