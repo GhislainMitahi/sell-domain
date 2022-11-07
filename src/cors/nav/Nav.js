1dc97b3a52cd4b7a8a15db38ad96d148b5e09519
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-07 14:18:07 +0200

    update nav

---
+++ src/cors/nav/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Root from '../../roots/routes';
+import logo from '../../images/log.png';
+import styles from './NavStyle.module.css';
+
+
+const Nav = () => {
+    console.log(styles);
+    return (
+        <div className={`h-[80px] w-full bg-slate-900 flex justify-center align-center ${styles.nav}`}>
+            <div className = {`w-[95%] border border-white flex justify-between items-center`}>
+                <p className = {`h-[100%] w-[15%] border border-white flex justify-center align-center`}>
+                    <img src={logo} alt="" />
+                </p>
+                <p className={`w-[85%] flex justify-around items-center`}>
+                    <Root />
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
