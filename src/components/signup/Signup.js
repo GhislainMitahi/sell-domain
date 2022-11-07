f2559a28d27c0e2d2fa5820ef3277abbc9028865
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-07 14:17:54 +0200

    update sign up

---
+++ src/components/signup/Signup.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import style from './NavStyle.module.css';
+import 
+
+const Signup = () => {
+  return (
+    <>
+      <div className={``}>
+        <div className={``}>
+          <p className={``}>
+             <span>Get start to Become a reseller</span>
+          </p>
+          <p className={``}>
+             <img src="" alt="" />
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Signup
